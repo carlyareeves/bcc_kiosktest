0e518934f4223fdcda31f25b64f71af8307ccce5
carlyareeves <28751528+carlyareeves@users.noreply.github.com>
2025-11-14 12:26:31 +1000

    Added app force reload button

---
+++ src/components/ButtonReload.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@mui/material";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+
+const ButtonReload: React.FC = () => {
+  const handleHardReload = () => {
+    window.location.reload();
+  };
+
+  return (
+    <>
+      <Button
+        onClick={handleHardReload}
+        variant="contained"
+        startIcon={<RestartAltIcon />}
+        sx={{
+          borderRadius: 999,
+          padding: "10px 20px",
+          textTransform: "none",
+          fontSize: "32px",
+          color: "white",
+          backgroundColor: "blue",
+          boxShadow: "none",
+          "&:hover": { boxShadow: "none", color: "white" },
+        }}
+      >
+        Reload
+      </Button>
+    </>
+  );
+};
+
+export default ButtonReload;
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,16 @@
-import Box from "@mui/material/Box";
+import { Box, Stack } from "@mui/material";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
+import ButtonReload from "../components/ButtonReload";
 
 const RootLayout = () => {
   return (
     <Box sx={{ overflow: "clip" }}>
       <div className="grid_wrapper">
-        <div className="promo_wrapper">Display Promo</div>
+        <Stack direction="column" gap="30px" className="promo_wrapper">
+          <div>Display Promo</div>
+          <ButtonReload />
+        </Stack>
         <div className="ui_wrapper">
           <Sidebar />
           <div className="content">
